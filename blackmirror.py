f6fde65359893233bffe1cebddcac7d92fd54834
mirkan <mirkanbaba1@gmail.com>
2018-02-18 12:56:33 +0300

    done1

---
+++ blackmirror.py
@@ -0,0 +1,30 @@
+import random
+
+def choose():
+	sezon = random.randint(1,4)
+	read_txt = open('black-mirror.txt', 'r')
+
+	if sezon == 1 or sezon == 2:
+		bolum = random.randint(1,3)
+	elif sezon == 3:
+		bolum = random.randint(0,6)
+	elif sezon == 4:
+		bolum = random.randint(1,6)
+
+	sonuc = 'S{}B{}'.format(sezon, bolum)
+		
+	split = read_txt.read().split()		
+	for i in split:
+		if i == sonuc:
+			read_txt.close()
+			print('varolan secildi')
+			return choose()
+
+	read_txt.close()
+	print(sonuc)
+	save_txt = open('black-mirror.txt', 'a')
+	save_txt.write(' {}'.format(sonuc))
+	save_txt.close()
+
+choose()
+
